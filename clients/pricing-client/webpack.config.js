bbb99530a69aae332b0b342b7d87f5ebe89320e8
João Pinho <j.pinho@epilot.cloud>
2022-01-30 14:44:14 +0000

    feat: adds pricing client

---
+++ clients/pricing-client/webpack.config.js
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/definition.ts',
+  mode: 'production',
+  resolve: { extensions: ['.ts'] },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '*.d.ts', context: './src' }],
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'definition.js',
+    libraryTarget: 'commonjs',
+  },
+};
---
+++ packages/epilot-sdk/src/pricing-client.ts
@@ -0,0 +1,1 @@
+export * from '@epilot/pricing-client';
