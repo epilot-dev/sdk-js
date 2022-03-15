b1199389bbdf43b41d75a449dd087bf7701de941
Sascha Just <s.just@epilot.cloud>
2022-03-15 15:11:07 +0100

    remove client

--- clients/ts-sam-client/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { getClient } from './client';
-
-const client = getClient();
-export default client;
-
-export type { Client, Components, Paths, PathsDictionary, OperationMethods } from './openapi';
-export type { OpenAPIClient, OpenAPIClientAxios, Document } from 'openapi-client-axios';
-
-export * from './client';
