a9a181597ec6e062bb6928fd588ab7517d658b85
Viljami Kuosmanen <v.kuosmanen@epilot.cloud>
2022-05-20 20:57:32 +0200

    Default export for entity-client

--- clients/entity-client/src/index.ts
+++ clients/entity-client/src/index.ts
@@ -1,4 +1,9 @@
+import { getClient } from './client';
+
 export * from './client';
 export * from './schema-model';
 export type { Client, Components, Paths, PathsDictionary, OperationMethods } from './openapi';
 export type { OpenAPIClient, OpenAPIClientAxios, Document } from 'openapi-client-axios';
+
+const client = getClient();
+export default client;
