a971e8920ebd4dca0510a8da08a0420f809bdb9d
Ankit Mohan <find.ankit@gmail.com>
2022-06-17 11:41:41 +0530

    feat: new_entity_state property on action

--- clients/entity-client/src/openapi.d.ts
+++ clients/entity-client/src/openapi.d.ts
@@ -2413,6 +2413,21 @@ declare namespace Components {
                  * Sets the action as the default action, visible as the main action button.
                  */
                 default?: boolean;
+                new_entity_state?: /**
+                 * example:
+                 * {
+                 *   "_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                 *   "_org": "123",
+                 *   "_schema": "contact",
+                 *   "_tags": [
+                 *     "example",
+                 *     "mock"
+                 *   ],
+                 *   "_created_at": "2021-02-09T12:41:43.662Z",
+                 *   "_updated_at": "2021-02-09T12:41:43.662Z"
+                 * }
+                 */
+                BaseEntity;
             }[];
             drawer_size?: "small" | "medium" | "large";
             summary_fields?: (string | /* Summary Fields are displayed inside list view as a resume of the relation entity. */ SummaryField)[];
