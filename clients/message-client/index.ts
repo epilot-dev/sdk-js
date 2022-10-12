97a92bb4e034fb4c1712b581323ab20500bf7999
Ty Nguyen <ty.nguyen@axonactive.com>
2022-10-12 10:30:10 +0000

    feat: remove unused file

--- clients/message-client/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './client';
-export type { Client, Components, Paths, PathsDictionary, OperationMethods } from './openapi';
-export type { OpenAPIClient, OpenAPIClientAxios, Document } from 'openapi-client-axios';
