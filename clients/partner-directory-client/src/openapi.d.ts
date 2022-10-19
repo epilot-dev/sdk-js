46cdeb014b8b4e6a24bfca491fb0588b22ec95fe
Nguyen Huu Ty <ty.nguyen@axonactive.com>
2022-10-19 17:10:09 +0700

    feat: add partner client

---
+++ clients/partner-directory-client/src/openapi.d.ts
@@ -0,0 +1,549 @@
+/* eslint-disable */
+import type {
+  OpenAPIClient,
+  Parameters,
+  UnknownParamsObject,
+  OperationResponse,
+  AxiosRequestConfig,
+} from 'openapi-client-axios'; 
+
+declare namespace Components {
+    namespace Schemas {
+        export interface ActivatePartnerPayload {
+            /**
+             * Company name
+             * example:
+             * Company name
+             */
+            company_name?: string;
+            /**
+             * Email using to sign up
+             */
+            signed_up_email: string; // email
+            /**
+             * organization id
+             */
+            organization_id: string;
+        }
+        export type Assignable = AssignableUser | AssignablePartnerUser | AssignableOrganization;
+        export interface AssignableOrganization {
+            /**
+             * example:
+             * partner_organization
+             */
+            type: "partner_organization";
+            /**
+             * example:
+             * Example Partner Organization
+             */
+            display_name: string;
+            image_uri?: {
+                /**
+                 * example:
+                 * https://epilot-staging-user-content.s3.eu-central-1.amazonaws.com/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                original: string; // uri
+                /**
+                 * example:
+                 * https://file.sls.epilot.io/v1/files/public/preview?w=32&h=32&key=/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                thumbnail_32?: string; // uri
+            };
+            org_id: /**
+             * example:
+             * 123
+             */
+            OrganizationId;
+            partner_id?: /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            PartnerId;
+        }
+        export interface AssignablePartnerUser {
+            /**
+             * example:
+             * partner_user
+             */
+            type: "partner_user";
+            /**
+             * example:
+             * Example Partner User
+             */
+            display_name: string;
+            image_uri?: {
+                /**
+                 * example:
+                 * https://epilot-staging-user-content.s3.eu-central-1.amazonaws.com/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                original: string; // uri
+                /**
+                 * example:
+                 * https://file.sls.epilot.io/v1/files/public/preview?w=32&h=32&key=/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                thumbnail_32?: string; // uri
+            };
+            org_id: /**
+             * example:
+             * 123
+             */
+            OrganizationId;
+            partner_id?: /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            PartnerId;
+            /**
+             * example:
+             * 456
+             */
+            user_id?: string;
+        }
+        export interface AssignableUser {
+            /**
+             * example:
+             * user
+             */
+            type: "user";
+            /**
+             * example:
+             * Example User
+             */
+            display_name: string;
+            image_uri?: {
+                /**
+                 * example:
+                 * https://epilot-staging-user-content.s3.eu-central-1.amazonaws.com/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                original: string; // uri
+                /**
+                 * example:
+                 * https://file.sls.epilot.io/v1/files/public/preview?w=32&h=32&key=/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                thumbnail_32?: string; // uri
+            };
+            org_id: /**
+             * example:
+             * 123
+             */
+            OrganizationId;
+            /**
+             * example:
+             * 456
+             */
+            user_id?: string;
+        }
+        export interface BaseAssignable {
+            /**
+             * example:
+             * user
+             */
+            type: string;
+            /**
+             * example:
+             * Example User
+             */
+            display_name: string;
+            image_uri?: {
+                /**
+                 * example:
+                 * https://epilot-staging-user-content.s3.eu-central-1.amazonaws.com/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                original: string; // uri
+                /**
+                 * example:
+                 * https://file.sls.epilot.io/v1/files/public/preview?w=32&h=32&key=/728/8043d909-71fc-4838-a363-1b15dc1d585c/epilot.png
+                 */
+                thumbnail_32?: string; // uri
+            };
+            org_id: /**
+             * example:
+             * 123
+             */
+            OrganizationId;
+        }
+        export type InviteToken = string;
+        /**
+         * example:
+         * 123
+         */
+        export type OrganizationId = string;
+        export interface Partner {
+            id?: /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            PartnerId;
+            organization_id?: /**
+             * example:
+             * 123
+             */
+            OrganizationId;
+            /**
+             * example:
+             * 2022-02-08T04:44:32.246Z
+             */
+            created_at?: string;
+            /**
+             * Description
+             * example:
+             * Description
+             */
+            description?: string;
+            /**
+             * Company name
+             * example:
+             * Company name
+             */
+            company_name?: string;
+            /**
+             * Invitation token
+             */
+            invitation_token?: string;
+            /**
+             * Email using to receive invitation
+             */
+            invitation_email?: string; // email
+            /**
+             * Email using to sign up
+             */
+            signed_up_email?: string; // email
+            /**
+             * Target Organization
+             * example:
+             * 123456
+             */
+            partner_org_id?: string;
+            status?: "Pending" | "Request" | "Deleted" | "Invited" | "Rejected";
+        }
+        /**
+         * example:
+         * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+         */
+        export type PartnerId = string;
+        export interface PartnerInvitationPayload {
+            /**
+             * Language for partner invitation email
+             */
+            language?: "en" | "de";
+        }
+    }
+}
+declare namespace Paths {
+    namespace ActivatePartner {
+        namespace Parameters {
+            export type Token = Components.Schemas.InviteToken;
+        }
+        export interface QueryParameters {
+            token: Parameters.Token;
+        }
+        export type RequestBody = Components.Schemas.ActivatePartnerPayload;
+        namespace Responses {
+            export interface $200 {
+            }
+            export interface $404 {
+            }
+        }
+    }
+    namespace ApprovePartner {
+        namespace Parameters {
+            export type Id = /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            Components.Schemas.PartnerId;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        namespace Responses {
+            export type $201 = Components.Schemas.Partner;
+            export interface $400 {
+            }
+        }
+    }
+    namespace GetPartnerByToken {
+        namespace Parameters {
+            export type Token = Components.Schemas.InviteToken;
+        }
+        export interface QueryParameters {
+            token: Parameters.Token;
+        }
+        namespace Responses {
+            export type $200 = Components.Schemas.Partner;
+            export interface $404 {
+            }
+        }
+    }
+    namespace InvitePartner {
+        namespace Parameters {
+            export type Id = /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            Components.Schemas.PartnerId;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        export type RequestBody = Components.Schemas.PartnerInvitationPayload;
+        namespace Responses {
+            export type $200 = Components.Schemas.Partner;
+            export interface $400 {
+            }
+        }
+    }
+    namespace RejectPartner {
+        namespace Parameters {
+            export type Id = /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            Components.Schemas.PartnerId;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        namespace Responses {
+            export type $200 = Components.Schemas.Partner;
+            export interface $400 {
+            }
+        }
+    }
+    namespace ResendPartnerInvitation {
+        namespace Parameters {
+            export type Id = /**
+             * example:
+             * e45a6dc2-3795-43a3-ae0f-6b6760f310fc
+             */
+            Components.Schemas.PartnerId;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        export interface RequestBody {
+            /**
+             * Language for partner invitation email
+             */
+            language?: "en" | "de";
+        }
+        namespace Responses {
+            export type $200 = Components.Schemas.Partner;
+            export interface $400 {
+            }
+        }
+    }
+    namespace SearchAssignable {
+        export interface RequestBody {
+            /**
+             * search query to filter results
+             */
+            q: string;
+            /**
+             * start results from an offset for pagination
+             */
+            from?: number;
+            /**
+             * limit number of results to return
+             */
+            size?: number;
+            /**
+             * filter results to specific organizations. defaults to all orgs
+             */
+            org_ids?: /**
+             * example:
+             * 123
+             */
+            Components.Schemas.OrganizationId[];
+            /**
+             * filter results to specific types of assignables. defaults to all types
+             */
+            types?: ("user" | "partner_user" | "partner_organization")[];
+        }
+        namespace Responses {
+            export interface $200 {
+                /**
+                 * total number of search results
+                 * example:
+                 * 25
+                 */
+                hits?: number;
+                results?: Components.Schemas.Assignable[];
+            }
+        }
+    }
+}
+
+export interface OperationMethods {
+  /**
+   * invitePartner - invitePartner
+   * 
+   * Create a new partner in partner directory and send an invite email to accept request
+   */
+  'invitePartner'(
+    parameters?: Parameters<Paths.InvitePartner.PathParameters> | null,
+    data?: Paths.InvitePartner.RequestBody,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.InvitePartner.Responses.$200>
+  /**
+   * approvePartner - approvePartner
+   * 
+   * Approve partner request
+   */
+  'approvePartner'(
+    parameters?: Parameters<Paths.ApprovePartner.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.ApprovePartner.Responses.$201>
+  /**
+   * rejectPartner - rejectPartner
+   * 
+   * Reject partner request
+   */
+  'rejectPartner'(
+    parameters?: Parameters<Paths.RejectPartner.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.RejectPartner.Responses.$200>
+  /**
+   * resendPartnerInvitation - resendPartnerInvitation
+   * 
+   * Resend partner invitation email
+   */
+  'resendPartnerInvitation'(
+    parameters?: Parameters<Paths.ResendPartnerInvitation.PathParameters> | null,
+    data?: Paths.ResendPartnerInvitation.RequestBody,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.ResendPartnerInvitation.Responses.$200>
+  /**
+   * searchAssignable - searchAssignables
+   * 
+   * Search for assignable users/organizations from this organization and Partners
+   * 
+   * Results can include:
+   *  - Users in your organization
+   *  - Users in partner organizations
+   *  - Partner organizations
+   * 
+   */
+  'searchAssignable'(
+    parameters?: Parameters<UnknownParamsObject> | null,
+    data?: Paths.SearchAssignable.RequestBody,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.SearchAssignable.Responses.$200>
+  /**
+   * getPartnerByToken - getPartnerByToken
+   * 
+   * Get partner by token
+   */
+  'getPartnerByToken'(
+    parameters?: Parameters<Paths.GetPartnerByToken.QueryParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.GetPartnerByToken.Responses.$200>
+  /**
+   * activatePartner - activatePartner
+   * 
+   * Activate partner using an invite token
+   */
+  'activatePartner'(
+    parameters?: Parameters<Paths.ActivatePartner.QueryParameters> | null,
+    data?: Paths.ActivatePartner.RequestBody,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.ActivatePartner.Responses.$200>
+}
+
+export interface PathsDictionary {
+  ['/v1/partners/{id}/invite']: {
+    /**
+     * invitePartner - invitePartner
+     * 
+     * Create a new partner in partner directory and send an invite email to accept request
+     */
+    'post'(
+      parameters?: Parameters<Paths.InvitePartner.PathParameters> | null,
+      data?: Paths.InvitePartner.RequestBody,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.InvitePartner.Responses.$200>
+  }
+  ['/v1/partners/{id}/approve']: {
+    /**
+     * approvePartner - approvePartner
+     * 
+     * Approve partner request
+     */
+    'post'(
+      parameters?: Parameters<Paths.ApprovePartner.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.ApprovePartner.Responses.$201>
+  }
+  ['/v1/partners/{id}/reject']: {
+    /**
+     * rejectPartner - rejectPartner
+     * 
+     * Reject partner request
+     */
+    'post'(
+      parameters?: Parameters<Paths.RejectPartner.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.RejectPartner.Responses.$200>
+  }
+  ['/v1/partners/{id}/invite:resendEmail']: {
+    /**
+     * resendPartnerInvitation - resendPartnerInvitation
+     * 
+     * Resend partner invitation email
+     */
+    'post'(
+      parameters?: Parameters<Paths.ResendPartnerInvitation.PathParameters> | null,
+      data?: Paths.ResendPartnerInvitation.RequestBody,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.ResendPartnerInvitation.Responses.$200>
+  }
+  ['/v1/partners/assignables:search']: {
+    /**
+     * searchAssignable - searchAssignables
+     * 
+     * Search for assignable users/organizations from this organization and Partners
+     * 
+     * Results can include:
+     *  - Users in your organization
+     *  - Users in partner organizations
+     *  - Partner organizations
+     * 
+     */
+    'post'(
+      parameters?: Parameters<UnknownParamsObject> | null,
+      data?: Paths.SearchAssignable.RequestBody,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.SearchAssignable.Responses.$200>
+  }
+  ['/v1/partner-directory/public/getPartnerByToken']: {
+    /**
+     * getPartnerByToken - getPartnerByToken
+     * 
+     * Get partner by token
+     */
+    'get'(
+      parameters?: Parameters<Paths.GetPartnerByToken.QueryParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.GetPartnerByToken.Responses.$200>
+  }
+  ['/v1/partner-directory/public/activate']: {
+    /**
+     * activatePartner - activatePartner
+     * 
+     * Activate partner using an invite token
+     */
+    'post'(
+      parameters?: Parameters<Paths.ActivatePartner.QueryParameters> | null,
+      data?: Paths.ActivatePartner.RequestBody,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.ActivatePartner.Responses.$200>
+  }
+}
+
+export type Client = OpenAPIClient<OperationMethods, PathsDictionary>
---
+++ packages/epilot-sdk/src/partner-directory-client.ts
@@ -0,0 +1,6 @@
+import { getClient } from '@epilot/partner-directory-client';
+
+const client = getClient();
+export default client;
+
+export * from '@epilot/partner-directory-client';
