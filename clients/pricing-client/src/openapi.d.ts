1d5beeebf919396e0b167762bda5059cbb3905ba
Alexandre Marques <a.marques@epilot.cloud>
2023-09-13 15:35:15 +0100

    chore: add currency to recurrence amount + bump pricing-client version

--- clients/pricing-client/src/openapi.d.ts
+++ clients/pricing-client/src/openapi.d.ts
@@ -2412,6 +2412,17 @@ declare namespace Components {
              * Total of all items taxes, with same recurrence.
              */
             amount_tax?: number;
+            /**
+             * Currency
+             */
+            currency?: /**
+             * Three-letter ISO currency code, in lowercase. Must be a supported currency.
+             * ISO 4217 CURRENCY CODES as specified in the documentation: https://www.iso.org/iso-4217-currency-codes.html
+             *
+             * example:
+             * EUR
+             */
+            Currency;
         }
         /**
          * An amount associated with a specific recurrence.
