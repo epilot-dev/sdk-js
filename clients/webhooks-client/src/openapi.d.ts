a2001a5713ac8f27b23b6b458a3dfa0e4d9f8411
Sebastian Sauerer <s.sauerer@epilot.cloud>
2023-11-22 10:37:24 +0100

    chore(webhook-client): update client version

--- clients/webhooks-client/src/openapi.d.ts
+++ clients/webhooks-client/src/openapi.d.ts
@@ -30,6 +30,14 @@ declare namespace Components {
             username: string;
             password?: string;
         }
+        /**
+         * Custom key/value pair of either type body, query or header
+         */
+        export interface CustomOAuthParameter {
+            type: "body" | "query" | "header";
+            key: string;
+            value: string;
+        }
         export interface ErrorResp {
             message?: string;
         }
@@ -209,6 +217,7 @@ declare namespace Components {
              */
             endpoint: string;
             httpMethod: HttpMethod;
+            customParameterList?: /* Custom key/value pair of either type body, query or header */ CustomOAuthParameter[];
         }
         /**
          * Configuration for the webhook payload
@@ -218,7 +227,6 @@ declare namespace Components {
             include_relations?: boolean;
             include_activity?: boolean;
             include_changed_attributes?: boolean;
-            include_relation_schemas?: string[];
         }
         /**
          * example:
