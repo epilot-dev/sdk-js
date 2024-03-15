63b22af0a60da1c0535ec529dbe2a6acb93f8419
Alexandre Marques <pt.alexandremarques@gmail.com>
2024-03-15 15:28:55 +0000

    chore: add getag types for priceitem

--- clients/pricing-client/src/openapi.d.ts
+++ clients/pricing-client/src/openapi.d.ts
@@ -1924,6 +1924,15 @@ declare namespace Components {
              */
             _tags?: string[];
         }
+        export interface PriceGetAg {
+            category: string;
+            markup_amount: number;
+            markup_amount_decimal: string;
+            unit_amount_gross: number;
+            unit_amount_gross_decimal?: string;
+            unit_amount_net: number;
+            unit_amount_net_decimal?: string;
+        }
         /**
          * example:
          * {
@@ -2125,6 +2134,7 @@ declare namespace Components {
              */
             pricing_model: "per_unit" | "tiered_graduated" | "tiered_volume" | "tiered_flatfee" | "external_getag";
             tiers_details?: TierDetails[];
+            get_ag?: PriceGetAg;
         }
         /**
          * Represents a price input to the pricing library.
