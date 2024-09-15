69818303cf10aad6589983e954ec354d1aec3a2f
Vlad Crișan <vlad.crishan20@gmail.com>
2024-09-15 23:35:54 +0300

    chore(automation-client): update client with new spec

--- clients/automation-client/src/openapi.d.ts
+++ clients/automation-client/src/openapi.d.ts
@@ -949,6 +949,7 @@ declare namespace Components {
                 attribute?: string;
                 attributeType?: "string" | "text" | "number" | "boolean" | "date" | "datetime" | "tags" | "country" | "email" | "phone" | "product" | "price" | "status" | "relation" | "multiselect" | "select" | "radio" | "relation_user" | "purpose" | "label";
                 attributeRepeatable?: boolean;
+                attributeOperation?: "all" | "updated" | "added" | "deleted";
             };
             operation?: "equals" | "not_equals" | "any_of" | "none_of" | "contains" | "not_contains" | "starts_with" | "ends_with" | "greater_than" | "less_than" | "greater_than_or_equals" | "less_than_or_equals" | "is_empty" | "is_not_empty";
             values?: string[];
