be07d9e96627f34e69458a0f5800c5eb2a478b49
Ernesto Resende <ernesto.resende@epilot.cloud>
2024-09-30 22:40:54 -0300

    chore(notes-client): bump client version

--- clients/notes-api/src/openapi.d.ts
+++ clients/notes-api/src/openapi.d.ts
@@ -195,59 +195,14 @@ declare namespace Components {
         }
         export interface NotePatchRequestBody {
             /**
-             * Entity ID of the Note entry
-             */
-            _id?: string;
-            /**
-             * ID of the Organization that owns this Note
-             */
-            _org?: string;
-            /**
-             * The Entity schema of this Note
-             */
-            _schema?: string;
-            /**
-             * The timestamp of when this Note was created
-             */
-            _created_at?: string; // date-time
-            /**
-             * The timestamp of when this Note was last updated
-             */
-            _updated_at?: string; // date-time
-            /**
-             * The Entity ID of the User that created this Note
-             */
-            _created_by?: /* The Entity ID of the User that created this Note */ string | number;
-            /**
-             * The Entity ID of the User that created this Note
-             */
-            created_by?: /* The Entity ID of the User that created this Note */ string | number;
-            /**
              * Tags associated with this Note
              */
             _tags?: string[];
-            /**
-             * Access Control List for this Note entry
-             */
-            _acl?: {
-                [name: string]: string[];
-            };
-            _owners?: {
-                org_id: string;
-                user_id: string;
-            }[];
-            context_entities?: {
-                $relation: {
-                    entity_id: string;
-                }[];
-            };
-            parent?: /* The Note's parent Note */ NoteEntityParent;
             comments?: /* A note Entity object cotaining Entity metadata and content in a LexicalNode format */ NoteEntity[];
             /**
              * The content of the Note
              */
             content?: string;
-            context_workflow_tasks?: string[];
             /**
              * The timestamp of when this Note was pinned
              */
@@ -259,7 +214,7 @@ declare namespace Components {
              */
             _tags?: string[];
             /**
-             * The Entity ID of the Entity this note will be related to
+             * The Entity ID of the Entity this Note will be related to
              */
             entity_id: string;
             /**
@@ -278,6 +233,10 @@ declare namespace Components {
              * The content of the Note
              */
             content?: string;
+            /**
+             * List of File Entity IDs attached to the Note
+             */
+            attachments?: string[];
         }
         export interface NotesGetRequestResponse {
             /**
@@ -292,10 +251,43 @@ declare namespace Components {
     }
 }
 declare namespace Paths {
+    namespace CreateFileAttachment {
+        namespace Parameters {
+            /**
+             * The Entity ID of the Note entry to attach files to
+             */
+            export type Id = string;
+        }
+        export interface PathParameters {
+            id: /* The Entity ID of the Note entry to attach files to */ Parameters.Id;
+        }
+        export interface QueryParameters {
+            id: /* The Entity ID of the Note entry to attach files to */ Parameters.Id;
+        }
+        namespace Responses {
+            export interface $200 {
+            }
+        }
+    }
     namespace CreateNote {
         export type RequestBody = Components.Schemas.NotePostRequestBody;
         namespace Responses {
             export type $200 = /* A note Entity object cotaining Entity metadata and content in a LexicalNode format */ Components.Schemas.NoteEntity;
+        }
+    }
+    namespace DeleteFileAttachment {
+        namespace Parameters {
+            /**
+             * The Entity ID of the Note entry to attach files to
+             */
+            export type Id = string;
+        }
+        export interface PathParameters {
+            id: /* The Entity ID of the Note entry to attach files to */ Parameters.Id;
+        }
+        namespace Responses {
+            export interface $204 {
+            }
         }
     }
     namespace DeleteNote {
@@ -439,6 +431,16 @@ export interface OperationMethods {
     config?: AxiosRequestConfig  
   ): OperationResponse<Paths.DeleteNote.Responses.$200>
   /**
+   * getNotesByContext - getNotesByContext
+   * 
+   * Given a `context_type`, returns a list of Notes that belong to that context within the specified `id`
+   */
+  'getNotesByContext'(
+    parameters?: Parameters<Paths.GetNotesByContext.QueryParameters & Paths.GetNotesByContext.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.GetNotesByContext.Responses.$200>
+  /**
    * pinNote - pinNote
    * 
    * Pins a single Note entry based on it's Entity ID
@@ -449,15 +451,25 @@ export interface OperationMethods {
     config?: AxiosRequestConfig  
   ): OperationResponse<Paths.PinNote.Responses.$200>
   /**
-   * getNotesByContext - getNotesByContext
+   * createFileAttachment - createFileAttachment
    * 
-   * Given a `context_type`, returns a list of Notes that belong to that context within the specified `id`
+   * Attaches a File to a Note
    */
-  'getNotesByContext'(
-    parameters?: Parameters<Paths.GetNotesByContext.QueryParameters & Paths.GetNotesByContext.PathParameters> | null,
+  'createFileAttachment'(
+    parameters?: Parameters<Paths.CreateFileAttachment.QueryParameters & Paths.CreateFileAttachment.PathParameters> | null,
     data?: any,
     config?: AxiosRequestConfig  
-  ): OperationResponse<Paths.GetNotesByContext.Responses.$200>
+  ): OperationResponse<Paths.CreateFileAttachment.Responses.$200>
+  /**
+   * deleteFileAttachment - deleteFileAttachment
+   * 
+   * Attaches a File to a Note
+   */
+  'deleteFileAttachment'(
+    parameters?: Parameters<Paths.DeleteFileAttachment.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.DeleteFileAttachment.Responses.$204>
 }
 
 export interface PathsDictionary {
@@ -505,18 +517,6 @@ export interface PathsDictionary {
       config?: AxiosRequestConfig  
     ): OperationResponse<Paths.DeleteNote.Responses.$200>
   }
-  ['/v1/note/{id}:pin']: {
-    /**
-     * pinNote - pinNote
-     * 
-     * Pins a single Note entry based on it's Entity ID
-     */
-    'post'(
-      parameters?: Parameters<Paths.PinNote.PathParameters> | null,
-      data?: any,
-      config?: AxiosRequestConfig  
-    ): OperationResponse<Paths.PinNote.Responses.$200>
-  }
   ['/v1/notes/{context_id}']: {
     /**
      * getNotesByContext - getNotesByContext
@@ -528,6 +528,40 @@ export interface PathsDictionary {
       data?: any,
       config?: AxiosRequestConfig  
     ): OperationResponse<Paths.GetNotesByContext.Responses.$200>
+  }
+  ['/v1/note/{id}/pin']: {
+    /**
+     * pinNote - pinNote
+     * 
+     * Pins a single Note entry based on it's Entity ID
+     */
+    'post'(
+      parameters?: Parameters<Paths.PinNote.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.PinNote.Responses.$200>
+  }
+  ['/v1/note/{id}/attachments']: {
+    /**
+     * createFileAttachment - createFileAttachment
+     * 
+     * Attaches a File to a Note
+     */
+    'post'(
+      parameters?: Parameters<Paths.CreateFileAttachment.QueryParameters & Paths.CreateFileAttachment.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.CreateFileAttachment.Responses.$200>
+    /**
+     * deleteFileAttachment - deleteFileAttachment
+     * 
+     * Attaches a File to a Note
+     */
+    'delete'(
+      parameters?: Parameters<Paths.DeleteFileAttachment.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.DeleteFileAttachment.Responses.$204>
   }
 }
 
