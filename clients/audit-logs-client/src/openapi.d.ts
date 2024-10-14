f758b0d507b4976fc44a1fa6a8dac36c7cfb7e2d
Sebastian Sauerer <s.sauerer@epilot.cloud>
2024-10-14 16:43:39 +0200

    chore(audit-log): add new types

--- clients/audit-logs-client/src/openapi.d.ts
+++ clients/audit-logs-client/src/openapi.d.ts
@@ -66,22 +66,67 @@ declare namespace Components {
              * example:
              * 2021-06-01T12:00:00Z
              */
-            timestamp?: string; // date-time
+            timestamp?: string;
             caller?: Caller;
-            http?: {
-                /**
-                 * HTTP method
-                 * example:
-                 * GET
-                 */
-                method?: string;
-            };
+            http?: HttpContext;
             /**
              * Contains the stringified request body
              * example:
              * {"workflow_id": "123456"}
              */
             detail?: string;
+        }
+        export interface HttpContext {
+            /**
+             * HTTP method
+             * example:
+             * GET
+             */
+            method?: string;
+            /**
+             * IP address of the caller
+             * example:
+             */
+            ip?: string;
+            /**
+             * User agent of the caller
+             * example:
+             * Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3
+             */
+            user_agent?: string;
+            /**
+             * HTTP headers
+             * example:
+             * {
+             *   "Authorization": "Bearer token"
+             * }
+             */
+            headers?: {
+                [key: string]: any;
+            };
+            /**
+             * Query parameters
+             * example:
+             * {
+             *   "limit": 50,
+             *   "page": 0
+             * }
+             */
+            query?: {
+                [key: string]: any;
+            };
+            /**
+             * Path of the request
+             * example:
+             * /v1/logs
+             */
+            path?: string;
+            /**
+             * Domain name of the request
+             * example:
+             * audit-logs.sls.epilot.io
+             */
+            domainName?: string;
         }
     }
 }
@@ -126,12 +171,12 @@ declare namespace Paths {
              */
             export type Page = number;
             export type ServiceName = string;
-            export type Timestamp = string; // date-time
+            export type Timestamp = string;
         }
         export interface QueryParameters {
             limit?: /* Maximum number of results to return. */ Parameters.Limit;
             page?: /* Page number to return. */ Parameters.Page;
-            timestamp?: Parameters.Timestamp /* date-time */;
+            timestamp?: Parameters.Timestamp;
             service_name?: Parameters.ServiceName;
             event_name?: Parameters.EventName;
             caller_user_email?: Parameters.CallerUserEmail;
@@ -226,3 +271,4 @@ export type Client = OpenAPIClient<OperationMethods, PathsDictionary>
 
 export type Caller = Components.Schemas.Caller;
 export type Event = Components.Schemas.Event;
+export type HttpContext = Components.Schemas.HttpContext;
