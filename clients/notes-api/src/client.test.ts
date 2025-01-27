9912575fcbbc20a49478058e063024f7c6f428da
SaraVieira <hey@iamsaravieira.com>
2025-01-27 14:05:14 +0000

    chore: test foxes

--- clients/notes-api/src/client.test.ts
+++ clients/notes-api/src/client.test.ts
@@ -1,25 +1,19 @@
-import { getClient } from './client';
+import { getClient } from "./client";
 
-describe('client', () => {
-  describe('getClient', () => {
-    it('should inititalise and return client', async () => {
+describe("client", () => {
+  describe("getClient", () => {
+    it("should inititalise and return client", async () => {
       const client = getClient();
 
       expect(client.api.initialized).toBe(true);
     });
 
-    it('should have operations', async () => {
+    it("should have operations", async () => {
       const client = getClient();
 
       const operations = client.api.getOperations();
 
       expect(operations.length).toBeGreaterThan(0);
-    });
-
-    it('should have a default server defined', async () => {
-      const client = getClient();
-
-      expect(client.defaults.baseURL).toBeDefined();
     });
   });
 });
