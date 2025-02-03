5dc7674e765caff85ae7996f0abb622189f2d5cd
Mateus Nardo <m.nardo@epilot.cloud>
2025-02-03 18:25:05 +0100

    chore(blueprint-manifest-api): bump api

--- clients/blueprint-manifest-client/src/openapi.d.ts
+++ clients/blueprint-manifest-client/src/openapi.d.ts
@@ -567,7 +567,7 @@ declare namespace Components {
             }[];
             changes?: PlanChanges;
         }
-        export type ResourceNodeType = "designbuilder" | "journey" | "product" | "price" | "tax" | "automation_flow" | "entity_mapping" | "file" | "emailtemplate" | "schema" | "schema_attribute" | "schema_capability" | "schema_group" | "schema_group_headline" | "workflow_definition" | "closing_reason" | "taxonomy_classification" | "webhook" | "custom_variable";
+        export type ResourceNodeType = "designbuilder" | "journey" | "product" | "price" | "tax" | "automation_flow" | "entity_mapping" | "file" | "emailtemplate" | "schema" | "schema_attribute" | "schema_capability" | "schema_group" | "schema_group_headline" | "workflow_definition" | "closing_reason" | "taxonomy_classification" | "webhook" | "custom_variable" | "coupon";
         export interface RootResourceNode {
             /**
              * ID of the resource
