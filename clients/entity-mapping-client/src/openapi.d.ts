0da3b735ee19b19c08a827dcffa865facbb9828d
David Munoz <d.leon@epilot.cloud>
2025-02-04 14:49:26 +0100

    chore(entity-mapping-client): update client with new spec

--- clients/entity-mapping-client/src/openapi.d.ts
+++ clients/entity-mapping-client/src/openapi.d.ts
@@ -338,7 +338,7 @@ declare namespace Components {
              */
             raw?: boolean;
         }
-        export type MappingSourceTargetType = "string" | "date" | "datetime" | "boolean" | "number" | "image" | "file" | "address" | "email" | "phone" | "select" | "multiselect";
+        export type MappingSourceTargetType = "string" | "date" | "datetime" | "boolean" | "number" | "image" | "file" | "address" | "email" | "phone" | "select" | "multiselect" | "payment";
         export interface MappingWarning {
             explanation: string;
             context?: string;
