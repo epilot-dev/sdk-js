61ad5d9854d9e2d409d5b198541d8e2eeaf2a1a1
Nishu Goel <n.goel@epilot.cloud>
2025-04-11 14:21:08 +0530

    chore(blueprint-manifest-client): update client with support for saved views

--- clients/blueprint-manifest-client/src/openapi.d.ts
+++ clients/blueprint-manifest-client/src/openapi.d.ts
@@ -578,7 +578,7 @@ declare namespace Components {
         /**
          * Type of the resource
          */
-        export type ResourceNodeType = "designbuilder" | "journey" | "product" | "price" | "tax" | "automation_flow" | "entity_mapping" | "file" | "emailtemplate" | "schema" | "schema_attribute" | "schema_capability" | "schema_group" | "schema_group_headline" | "workflow_definition" | "closing_reason" | "taxonomy_classification" | "webhook" | "custom_variable" | "coupon" | "usergroup";
+        export type ResourceNodeType = "designbuilder" | "journey" | "product" | "price" | "tax" | "automation_flow" | "entity_mapping" | "file" | "emailtemplate" | "schema" | "schema_attribute" | "schema_capability" | "schema_group" | "schema_group_headline" | "workflow_definition" | "closing_reason" | "taxonomy_classification" | "webhook" | "custom_variable" | "coupon" | "usergroup" | "saved_view";
         export interface ResourceReplacement {
             /**
              * Original resource ID to be replaced
