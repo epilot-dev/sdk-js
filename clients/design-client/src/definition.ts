b919ca7f4feb6ea7115c9f1a84252d296b133951
Viljami <v.kuosmanen@epilot.cloud>
2025-06-12 17:17:58 +0300

    Upgrade linter

--- clients/design-client/src/definition.ts
+++ clients/design-client/src/definition.ts
@@ -2,4 +2,4 @@ import type { Document } from 'openapi-client-axios';
 
 import definition from './openapi-runtime.json';
 
-export default (definition as unknown) as Document;
+export default definition as unknown as Document;
