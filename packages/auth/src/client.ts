62134b76d790c20d7784c9261f99ce8ee8859c04
Viljami <v.kuosmanen@epilot.cloud>
2025-06-12 17:17:58 +0300

    Upgrade linter

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -2,12 +2,14 @@ import { AxiosInstance } from 'openapi-client-axios';
 
 import { OAuthTokens } from './types';
 
-export const authorizeClient = (tokens: OAuthTokens) => <T extends AxiosInstance>(client: T): T => {
-  client.interceptors.request.use((request) => {
-    request.headers['authorization'] = `Bearer ${tokens.access_token}`;
+export const authorizeClient =
+  (tokens: OAuthTokens) =>
+  <T extends AxiosInstance>(client: T): T => {
+    client.interceptors.request.use((request) => {
+      request.headers['authorization'] = `Bearer ${tokens.access_token}`;
 
-    return request;
-  });
+      return request;
+    });
 
-  return client;
-};
+    return client;
+  };
