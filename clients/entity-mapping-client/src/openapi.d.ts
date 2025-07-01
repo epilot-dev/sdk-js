d52955c3a4d01bccecaf17ab8a28e796489bfa33
Dustin Hirten <d.hirten@epilot.cloud>
2025-07-01 15:30:19 +0200

    chore(entity-mapping-client): update client with new spec

--- clients/entity-mapping-client/src/openapi.d.ts
+++ clients/entity-mapping-client/src/openapi.d.ts
@@ -346,7 +346,7 @@ declare namespace Components {
              */
             raw?: boolean;
         }
-        export type MappingSourceTargetType = "string" | "date" | "datetime" | "boolean" | "number" | "image" | "file" | "address" | "email" | "phone" | "select" | "multiselect" | "payment" | "link";
+        export type MappingSourceTargetType = "string" | "date" | "datetime" | "boolean" | "number" | "image" | "file" | "address" | "email" | "phone" | "select" | "multiselect" | "payment" | "link" | "currency";
         export interface MappingWarning {
             explanation: string;
             context?: string;
