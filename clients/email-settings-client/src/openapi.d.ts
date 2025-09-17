c6b7e706aab2fa1ceaf228009e0c7a561f35964e
Aleksandr Lapuskin <aleksandr.lapuskin@epilot.cloud>
2025-09-17 17:25:30 +0300

    chore(email-settings-client): update client with new spec

--- clients/email-settings-client/src/openapi.d.ts
+++ clients/email-settings-client/src/openapi.d.ts
@@ -9,7 +9,33 @@ import type {
 } from 'openapi-client-axios';
 
 declare namespace Components {
+    namespace Parameters {
+        export type SharedInboxId = string;
+    }
+    export interface PathParameters {
+        SharedInboxId?: Parameters.SharedInboxId;
+    }
+    namespace Responses {
+        export type BadRequest = Schemas.ErrorResponse;
+        export type Conflict = Schemas.ErrorResponse;
+        export type CreateSharedInboxSuccessResponse = Schemas.SharedInboxResponse;
+        export type Forbidden = Schemas.ErrorResponse;
+        export type GetSharedInboxSuccessResponse = Schemas.SharedInboxResponse;
+        export type InternalServerError = Schemas.ErrorResponse;
+        export type ListSharedInboxesSuccessResponse = Schemas.SharedInboxResponse[];
+        export interface NoContent {
+        }
+        export type NotFound = Schemas.ErrorResponse;
+        export type UpdateSharedInboxSuccessResponse = Schemas.SharedInboxResponse;
+    }
     namespace Schemas {
+        export interface CreateSharedInboxPayload {
+            id?: string;
+            color: string;
+            name: string;
+            assignees?: string[];
+            description?: string;
+        }
         export interface Domain {
             /**
              * example:
@@ -25,6 +51,10 @@ declare namespace Components {
          * Setting that allows to add a custom domain. For e.g; doe.com
          */
         export type EmailDomainSetting = "email_domain";
+        export interface ErrorResponse {
+            error: string;
+            status: number;
+        }
         /**
          * - Restrict duplicates within:
          *   * 10s
@@ -52,6 +82,13 @@ declare namespace Components {
             created_by?: string;
             updated_by?: string;
         }
+        export interface SettingMeta {
+            id: string;
+            created_at: string; // date-time
+            updated_at?: string; // date-time
+            created_by?: string;
+            updated_by?: string;
+        }
         export type SettingType = /* Setting that allows to add a signature. */ SignatureSetting | /* Setting that allows to add a custom domain. For e.g; doe.com */ EmailDomainSetting | /* Setting that allows to add an email address on the custom domain. For e.g; john@doe.com */ EmailAddressSetting | /**
          * - Setting that specifies a list of addresses exempt from being flagged as duplicate emails.
          * - An email will be flagged as a duplicate if it has the same content and is sent to the same recipient within the time frame specified in the RestrictDuplicatesWithinSetting.
@@ -72,10 +109,27 @@ declare namespace Components {
          */
         RestrictDuplicatesWithinSetting;
         export type SettingsResponse = Setting[] | Setting;
+        export interface SharedInboxResponse {
+            id: string;
+            created_at: string; // date-time
+            updated_at?: string; // date-time
+            created_by?: string;
+            updated_by?: string;
+            name: string;
+            color: string;
+            assignees: string[];
+            description?: string;
+        }
         /**
          * Setting that allows to add a signature.
          */
         export type SignatureSetting = "signature";
+        export interface UpdateSharedInboxPayload {
+            color?: string;
+            name?: string;
+            assignees?: string[];
+            description?: string;
+        }
         /**
          * - Setting that specifies a list of addresses exempt from being flagged as duplicate emails.
          * - An email will be flagged as a duplicate if it has the same content and is sent to the same recipient within the time frame specified in the RestrictDuplicatesWithinSetting.
@@ -103,6 +157,16 @@ declare namespace Paths {
             }
             export interface $404 {
             }
+        }
+    }
+    namespace AddSharedInbox {
+        export type RequestBody = Components.Schemas.CreateSharedInboxPayload;
+        namespace Responses {
+            export type $201 = Components.Responses.CreateSharedInboxSuccessResponse;
+            export type $400 = Components.Responses.BadRequest;
+            export type $403 = Components.Responses.Forbidden;
+            export type $409 = Components.Responses.Conflict;
+            export type $500 = Components.Responses.InternalServerError;
         }
     }
     namespace DeleteDomain {
@@ -134,6 +198,19 @@ declare namespace Paths {
             }
         }
     }
+    namespace DeleteSharedInbox {
+        namespace Parameters {
+            export type Id = string;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        namespace Responses {
+            export type $204 = Components.Responses.NoContent;
+            export type $403 = Components.Responses.Forbidden;
+            export type $500 = Components.Responses.InternalServerError;
+        }
+    }
     namespace GetSettings {
         namespace Parameters {
             export type Id = string;
@@ -151,6 +228,27 @@ declare namespace Paths {
             }
         }
     }
+    namespace GetSharedInbox {
+        namespace Parameters {
+            export type Id = string;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        namespace Responses {
+            export type $200 = Components.Responses.GetSharedInboxSuccessResponse;
+            export type $403 = Components.Responses.Forbidden;
+            export type $404 = Components.Responses.NotFound;
+            export type $500 = Components.Responses.InternalServerError;
+        }
+    }
+    namespace ListSharedInboxes {
+        namespace Responses {
+            export type $200 = Components.Responses.ListSharedInboxesSuccessResponse;
+            export type $403 = Components.Responses.Forbidden;
+            export type $500 = Components.Responses.InternalServerError;
+        }
+    }
     namespace UpdateSetting {
         namespace Parameters {
             export type Id = string;
@@ -165,6 +263,23 @@ declare namespace Paths {
             }
             export interface $404 {
             }
+        }
+    }
+    namespace UpdateSharedInbox {
+        namespace Parameters {
+            export type Id = string;
+        }
+        export interface PathParameters {
+            id: Parameters.Id;
+        }
+        export type RequestBody = Components.Schemas.UpdateSharedInboxPayload;
+        namespace Responses {
+            export type $200 = Components.Responses.UpdateSharedInboxSuccessResponse;
+            export type $400 = Components.Responses.BadRequest;
+            export type $403 = Components.Responses.Forbidden;
+            export type $404 = Components.Responses.NotFound;
+            export type $409 = Components.Responses.Conflict;
+            export type $500 = Components.Responses.InternalServerError;
         }
     }
     namespace VerifyDomain {
@@ -191,6 +306,56 @@ declare namespace Paths {
 
 
 export interface OperationMethods {
+  /**
+   * getSharedInbox - getSharedInbox
+   * 
+   * Get shared inbox
+   */
+  'getSharedInbox'(
+    parameters?: Parameters<Paths.GetSharedInbox.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.GetSharedInbox.Responses.$200>
+  /**
+   * updateSharedInbox - updateSharedInbox
+   * 
+   * Update shared inbox
+   */
+  'updateSharedInbox'(
+    parameters?: Parameters<Paths.UpdateSharedInbox.PathParameters> | null,
+    data?: Paths.UpdateSharedInbox.RequestBody,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.UpdateSharedInbox.Responses.$200>
+  /**
+   * deleteSharedInbox - deleteSharedInbox
+   * 
+   * Delete shared inbox
+   */
+  'deleteSharedInbox'(
+    parameters?: Parameters<Paths.DeleteSharedInbox.PathParameters> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.DeleteSharedInbox.Responses.$204>
+  /**
+   * listSharedInboxes - listSharedInboxes
+   * 
+   * List shared inboxes
+   */
+  'listSharedInboxes'(
+    parameters?: Parameters<UnknownParamsObject> | null,
+    data?: any,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.ListSharedInboxes.Responses.$200>
+  /**
+   * addSharedInbox - addSharedInbox
+   * 
+   * Add shared inbox
+   */
+  'addSharedInbox'(
+    parameters?: Parameters<UnknownParamsObject> | null,
+    data?: Paths.AddSharedInbox.RequestBody,
+    config?: AxiosRequestConfig  
+  ): OperationResponse<Paths.AddSharedInbox.Responses.$201>
   /**
    * getSettings - getSettings
    * 
@@ -274,6 +439,60 @@ export interface OperationMethods {
 }
 
 export interface PathsDictionary {
+  ['/v2/email-settings/shared-inboxes/{id}']: {
+    /**
+     * getSharedInbox - getSharedInbox
+     * 
+     * Get shared inbox
+     */
+    'get'(
+      parameters?: Parameters<Paths.GetSharedInbox.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.GetSharedInbox.Responses.$200>
+    /**
+     * deleteSharedInbox - deleteSharedInbox
+     * 
+     * Delete shared inbox
+     */
+    'delete'(
+      parameters?: Parameters<Paths.DeleteSharedInbox.PathParameters> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.DeleteSharedInbox.Responses.$204>
+    /**
+     * updateSharedInbox - updateSharedInbox
+     * 
+     * Update shared inbox
+     */
+    'put'(
+      parameters?: Parameters<Paths.UpdateSharedInbox.PathParameters> | null,
+      data?: Paths.UpdateSharedInbox.RequestBody,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.UpdateSharedInbox.Responses.$200>
+  }
+  ['/v2/email-settings/shared-inboxes']: {
+    /**
+     * listSharedInboxes - listSharedInboxes
+     * 
+     * List shared inboxes
+     */
+    'get'(
+      parameters?: Parameters<UnknownParamsObject> | null,
+      data?: any,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.ListSharedInboxes.Responses.$200>
+    /**
+     * addSharedInbox - addSharedInbox
+     * 
+     * Add shared inbox
+     */
+    'post'(
+      parameters?: Parameters<UnknownParamsObject> | null,
+      data?: Paths.AddSharedInbox.RequestBody,
+      config?: AxiosRequestConfig  
+    ): OperationResponse<Paths.AddSharedInbox.Responses.$201>
+  }
   ['/v1/email-settings']: {
     /**
      * getSettings - getSettings
@@ -369,12 +588,17 @@ export interface PathsDictionary {
 export type Client = OpenAPIClient<OperationMethods, PathsDictionary>
 
 
+export type CreateSharedInboxPayload = Components.Schemas.CreateSharedInboxPayload;
 export type Domain = Components.Schemas.Domain;
 export type EmailAddressSetting = Components.Schemas.EmailAddressSetting;
 export type EmailDomainSetting = Components.Schemas.EmailDomainSetting;
+export type ErrorResponse = Components.Schemas.ErrorResponse;
 export type RestrictDuplicatesWithinSetting = Components.Schemas.RestrictDuplicatesWithinSetting;
 export type Setting = Components.Schemas.Setting;
+export type SettingMeta = Components.Schemas.SettingMeta;
 export type SettingType = Components.Schemas.SettingType;
 export type SettingsResponse = Components.Schemas.SettingsResponse;
+export type SharedInboxResponse = Components.Schemas.SharedInboxResponse;
 export type SignatureSetting = Components.Schemas.SignatureSetting;
+export type UpdateSharedInboxPayload = Components.Schemas.UpdateSharedInboxPayload;
 export type WhitelistEmailAddressSetting = Components.Schemas.WhitelistEmailAddressSetting;
